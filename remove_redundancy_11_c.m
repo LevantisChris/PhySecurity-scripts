1be577b9b2ab7495091aacc343333e46613607ad
Levantis Christos <117188793+LevantisChris@users.noreply.github.com>
2024-07-02 19:32:50 +0300

    Remove redundancy DONE

---
+++ remove_redundancy_11_c.m
@@ -0,0 +1,22 @@
+function remove_redundancy_11_c() 
+    
+    load('decoded_messages_de_Hamming_11b.mat', 'decoded_messages');
+    num_messages = size(decoded_messages, 1);
+    for idx = 1:num_messages
+        received_message = decoded_messages(idx, :);
+        for i = 1:8
+            seven_bit_segment = received_message((i-1)*7 + 1 : i*7); % 7-bit segment
+            final_segment = remove_parity_bits(seven_bit_segment); % Καλούμε την συνάρτηση
+            final_segments((i-1)*4 + 1 : i*4) = final_segment; % Τώρα θα είναι 4-bit (7 - 3)
+        end
+        unRedundancy_messages(idx, :) = final_segments;
+    end
+    save('unRedundancy_messages_11c.mat', 'unRedundancy_messages');
+end
+
+function seven_bit_segment = remove_parity_bits(seven_bit_segment)
+    %             Π3 Π2 Π1 'Ε2' Π0 'Ε1' 'Ε0' --> 7 - 3 = 4-bit
+    % example:    1  1  0   0   1   1    0 --> 1 1 0 1
+    seven_bit_segment([7, 6, 4]) = [];
+    %disp(seven_bit_segment);
+end
--- test.m
+++ test.m
@@ -1,59 +1,20 @@
-function test(demodulated_messages)
+function test(received_message)
     
-    received_message = demodulated_messages;
-        % Κάνουμε Hamming decoding για κάθε 7-bit κομμάτι (Hamming (7,4)).
-        % Στην κωδικοποιήση (7, 4) από 4bit λαμβάνουμε 7bit άρα τώρα θα
-        % κάνουμε το ανάποδο, NOTE: 7-bit -> 
-        % Π3 Π2 Π1 Ε2 Π0 Ε1 Ε0
-        % Π3 --> segment(1)
-        % Π2 --> segment(2)
-        % Π1 --> segment(3)
-        % Π0 --> segment(5)
-        % E0 --> segment(7)
-        % E1 --> segment(6)
-        % E2 --> segment(4)
-        
-        % Τα μηνύματα είναι 58-bits άρα θα έχουμε 58/7 = 8 ζευγαράκια
-        for i = 1:8
-            % Παίρνουμε το 7-bit μέρος από όλο το μήνυμα που είναι 56-bits
-            segment = received_message((i-1)*7 + 1 : i*7);
-
-            % Τώρα πρέπει να υπολογίσουμε το σύνδρομο
-            % Z0 = E0 xor Π0 xor Π1 xor Π3
-            % Z1 = E1 xor Π0 xor Π2 xor Π3
-            % Ζ2 = Ε2 xor Π1 xor Π2 xor Π3
-            Z0 = xor( ...
-                xor(segment(7), segment(5)), ...
-                xor(segment(3), segment(1)));
-            Z1 = xor( ...
-                xor(segment(6), segment(5)), ...
-                xor(segment(2), segment(1)));
-            Z2 = xor( ...
-                xor(segment(4), segment(3)), ...
-                xor(segment(2), segment(1)));
-            S = strcat(num2str(Z2), num2str(Z1), num2str(Z0));
-            position = bin2dec(S); % Η θέση που το λάθος εντοπίστηκε, αυτό θα διορθώσουμε
-            disp(segment);
-            disp(S)
-            disp(position)
-            correct_segment = recplaceError(segment, position);
-            disp(correct_segment);
-            disp("----------------------")
-            corrected_message((i-1)*7 + 1 : i*7) = correct_segment;
-        end
-
-        disp(corrected_message);
-
+    %load('decoded_messages_de_Hamming_11b.mat', 'decoded_messages');
+    
+    %received_message = demodulated_messages(idx, :);
+    for i = 1:8
+        seven_bit_segment = received_message((i-1)*7 + 1 : i*7); % 7-bit segment
+        final_segment = remove_parity_bits(seven_bit_segment); % Καλούμε την συνάρτηση
+        final_segments((i-1)*4 + 1 : i*4) = final_segment; % Τώρα θα είναι 4-bit (7 - 3)
+    end
+    unRedundancy_messages(1, :) = final_segments;
+    disp(unRedundancy_messages);
 end
 
-function [new_segment] = recplaceError(segment, position)
-    if position ~= 0 % Αν έχουμε 0 προφανώς δεν υπάρχει λάθος
-        current_bit = segment(8 - position); % Πάρε το τωρινό bit (0 ή 1) που έχει το λάθος. θέλουμε το LSB για αυτό κάνω 7 - ...
-        if current_bit == 0
-            segment(8 - position) = 1; % Αυτή η γραμμή έχει διορθωθεί από 0 σε 1 με σκοπό να φτιαχτεί το error
-        else
-            segment(8 - position) = 0; % Αυτή η γραμμή έχει διορθωθεί από 1 σε 0 με σκοπό να φτιαχτεί το error
-        end
-    end
-    new_segment = segment; % νέο "διορθωμένο" segment
+function seven_bit_segment = remove_parity_bits(seven_bit_segment)
+    %             Π3 Π2 Π1 'Ε2' Π0 'Ε1' 'Ε0' --> 7 - 3 = 4-bit
+    % example:    1  1  0   0   1   1    0 --> 1 1 0 1
+    seven_bit_segment([7, 6, 4]) = [];
+    disp(seven_bit_segment);
 end
